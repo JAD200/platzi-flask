375a95d1bebb6229b29d617957fe5a0765779d91
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-07-28 21:01:36 -0300

    Redirection with request and response

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
+    
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
     
     return f'Hello world Platzi, tu IP es {user_ip}'

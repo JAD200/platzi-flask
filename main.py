4f83868176a5f33a33d902e74b0df125ab6bc556
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-07-29 23:27:45 -0300

    Use of static files -- Images

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, make_response, redirect, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates', static_folder='./static')
 
 todos = ['Comprar cafe', 'Comprar guantes', 'Hacer el pull request']
 

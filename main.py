b26b7a883690c2db625deb961f74b9471f329ea9
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-08-01 16:35:34 -0300

    Use of POST method in Flask-WTF

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-#   Bootstrap
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
-#   Flask
-from flask import Flask, make_response, redirect, render_template, request, session
+from flask import Flask, make_response, redirect, render_template, request, session, url_for
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
 bootstrap = Bootstrap(app)
@@ -10,6 +11,12 @@ bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = '69E9FBBE-27B3-4E68-8BC3-2D9EE573FFEF'
 
 todos = ['Comprar cafe', 'Comprar guantes', 'Hacer el pull request']
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -39,7 +46,7 @@ def index():
 
 
 #   Route shown if there is a cookie with the user's IP available
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     """hello Defines the data for this path
 
@@ -47,9 +54,19 @@ def hello():
         html: Template on Jira 2 with the route information
     """
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todos': todos,
+        'login_form': login_form,
+        'username': username,
     }
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)

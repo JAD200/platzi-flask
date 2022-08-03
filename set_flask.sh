c6f31733bf2df7409d34ae862954fda84c617a78
Juan Agustin Di Pasquo <juandipasquo@gmail.com>
2022-08-03 14:49:27 -0300

    Improve -- Flask configuration

--- set_flask.sh
+++ set_flask.sh
@@ -1,7 +1,13 @@
+divider================
+divider=$divider$divider
+
+width=22
+
 export FLASK_APP=main.py
 echo $FLASK_APP
+
 export FLASK_ENV=development
 echo $FLASK_ENV
-echo "
-Flask's sets are ready"
-echo "use   flask run --reload  to run the server"
+
+printf "%$width.${width}s\n\n" "$divider"
+printf "%s\n" "Flask's sets are ready" "use   flask run --reload  to run the server"
